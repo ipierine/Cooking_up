ea5599ce918a8fcd27c88748952e00a7f8690c69
Igor Leite Pierine dos Santos <92900641+ipierine@users.noreply.github.com>
2024-02-05 17:30:34 -0300

    Add files via upload

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import Tag from './Tag.vue';
+
+export default {
+  components: { Tag },
+  props: {
+    ingrediente: { type: String, required: true }
+  },
+  data() {
+    return {
+      selecionado: false
+    }
+  },
+  methods: {
+    aoClicar() {
+      this.selecionado = !this.selecionado
+
+      if (this.selecionado) {
+        this.$emit('adicionarIngrediente', this.ingrediente)
+      } else {
+        this.$emit('removerIngrediente', this.ingrediente);
+      }
+    }
+  },
+  emits: ['adicionarIngrediente', 'removerIngrediente']
+}
+</script>
+
+<template>
+  <button
+    class="ingrediente"
+    @click="aoClicar"
+    :aria-pressed="selecionado"
+  >
+    <Tag :texto="ingrediente" :ativa="selecionado" />
+  </button>
+</template>
+
+<style scoped>
+
+.ingrediente {
+    cursor: pointer;
+}
+
+.tag:hover {
+  background-color: var(--coral, #F0633C);
+  /* opacity: 0.8; */
+}
+
+</style>
+
